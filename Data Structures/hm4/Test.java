3a512296f09ce5519cda3d064b4d589d0b73417c
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-09-29 18:32:56 -0500

    init hw4

--- Data Structures/hm4/Test.java
+++ Data Structures/hm4/Test.java
@@ -1,5 +1,6 @@
 public class Test {
-    public static void main(String[] args) {
-        Stack<Integer> st = new Stack<>();
-    }
+  public static void main(String[] args) {
+    // Stack<Integer> st = new Stack<>();
+
+  }
 }
