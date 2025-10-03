ee497cfdec7a4f0c09d6e6363be58329bdb9d3a3
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-10-02 19:55:14 -0500

    hw4

--- Data Structures/hm4/given/Queue.java
+++ Data Structures/hm4/given/Queue.java
@@ -1,6 +1,9 @@
 public class Queue<T extends Comparable<T>> extends Structure<T> {
   public T pop() {
-    return;
+    Node<T> ret = this.head.next;
+    this.head = this.head.next;
+    this.head.before.next = null;
+    return ret.x;
   }
 
 }
