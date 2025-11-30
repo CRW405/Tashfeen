b3d2442a8834274e589dd39be5c095eb838cb4d3
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-11-29 22:05:47 -0600

    finshes oily 9

---
+++ Data Structures/f23csci2114hw7wiyninger/Wiyninger7.java
@@ -0,0 +1,33 @@
+public class Wiyninger7 {
+  // 10001st Prime
+  //
+  // 104743
+  public static void main(String[] args) {
+    int n = 10001;
+
+    boolean[] sieve = new boolean[n * 20];
+
+    for (int i = 2; i < sieve.length; i++) {
+      sieve[i] = true;
+    }
+
+    for (int i = 2; i * i < sieve.length; i++) {
+      if (sieve[i]) {
+        for (int j = i * i; j < sieve.length; j += i) {
+          sieve[j] = false;
+        }
+      }
+    }
+
+    int count = 0;
+    for (int i = 2; i < sieve.length; i++) {
+      if (sieve[i]) {
+        count++;
+        if (count == n) {
+          System.out.println(i);
+          break;
+        }
+      }
+    }
+  }
+}
