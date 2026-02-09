e457efbb0f3169b6b03bb56bf7b4d902d20a7dac
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-02-09 17:10:40 -0600

    ml3 donw

--- machine learning/hw3/bayes.py
+++ machine learning/hw3/bayes.py
@@ -8,24 +8,29 @@ class NaiveBayes:
         self.Y = y
 
     def predict(self, x):
-        return np.argmax([self.pr_y_given_x(0, x), self.pr_y_given_x(1, x)])
+        labels = np.unique(self.Y)
+        prob = [self.pr_y_given_x(y, x) for y in labels]
+        return np.argmax(prob)
 
     def pr_y_given_x(self, y, x):
-        # ???
-        log_prob = np.log(self.pr_y(y))
+        loggies = np.zeros(x.shape)
         for i, xi in enumerate(x):
-            log_prob += np.log(self.pr_xi_given_y(xi, i, y))
-        return log_prob
+            loggies[i] = np.log(self.pr_xi_given_y(xi, i, y))
+        return np.sum(loggies) + np.log(self.pr_y(y))
 
     def pr_xi_given_y(self, xi, i, y):
         # probability of xi at i given y
-        rows_with_y = self.X[self.Y == y]
+        rows_with_y = self.X[self.Y == y]  # filtered
 
         count = np.sum(rows_with_y[:, i] == xi)
 
         total = rows_with_y.shape[0]
 
-        return count / total
+        probability = count / total
+
+        return (
+            probability if probability > 0 else 2 ** (-32)
+        )  # if zero, then do really small number to avoid future log(0) error cause we're loop racist
 
     def pr_y(self, y):
         # probability of y
@@ -34,6 +39,7 @@ class NaiveBayes:
 
 model = NaiveBayes(X, y)
 if __name__ == "__main__":
+    print(model.predict(np.array([3, 1, 20, 2])))
     preds = [model.predict(x) for x in X_]
     accuracy = np.sum(preds == y_) / y_.shape[0]
     print(f"Accuracy: {accuracy}")
